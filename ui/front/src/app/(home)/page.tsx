d6204ac69dc9749dfb303bdfef408821722a07c6
awesomeYG <gang.yang@chaitin.com>
2025-09-10 19:00:22 +0800

    fix: add getDiscussion filter

--- ui/front/src/app/(home)/page.tsx
+++ ui/front/src/app/(home)/page.tsx
@@ -7,15 +7,18 @@ export const metadata: Metadata = {
   title: '技术讨论 | 长亭百川云',
 };
 
-const Page = async (props: { searchParams: Promise<{ search: string }> }) => {
+const Page = async (props: {
+  searchParams: Promise<{ search: string; sort: string }>;
+}) => {
   const searchParams = await props.searchParams;
-  const { search } = searchParams;
-
+  const { search, sort } = searchParams;
+  console.log(search, sort);
   const data = await getDiscussion({
     page: 1,
     size: 10,
     keyword: search,
-  })
+    filter: sort as any,
+  });
   return (
     <Stack gap={3} sx={{ minHeight: '100vh' }}>
       <h1 style={{ display: 'none' }}>技术评论</h1>
