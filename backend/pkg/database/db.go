c3f615e23ccd34a13ad04383b7bd7c144ce16fcf
姚凯 <kai.yao@chaitin.com>
2025-09-10 17:57:54 +0800

    feat: custom gorm logger

--- backend/pkg/database/db.go
+++ backend/pkg/database/db.go
@@ -6,7 +6,7 @@ import (
 	"github.com/chaitin/koalaqa/pkg/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	gormLogger "gorm.io/gorm/logger"
 )
 
 type DB = gorm.DB
@@ -15,14 +15,14 @@ type Scope = func(*DB) *DB
 
 var ErrRecordNotFound = gorm.ErrRecordNotFound
 
-func NewDB(cfg config.Config) (*DB, error) {
+func newDB(cfg config.Config, logger gormLogger.Interface) (*DB, error) {
 	gormConfig := &gorm.Config{
 		AllowGlobalUpdate: false,
-		Logger:            logger.Default.LogMode(logger.Silent),
+		Logger:            logger.LogMode(gormLogger.Silent),
 	}
 
 	if cfg.DB.Debug {
-		gormConfig.Logger = logger.Default.LogMode(logger.Info)
+		gormConfig.Logger = logger.LogMode(gormLogger.Info)
 	}
 
 	db, err := gorm.Open(postgres.New(postgres.Config{
