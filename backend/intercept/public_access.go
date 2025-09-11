159d7986c68ca18edf76980e9e581e418445db29
姚凯 <kai.yao@chaitin.com>
2025-09-11 16:51:06 +0800

    fix: public access freeauth

--- backend/intercept/public_access.go
+++ backend/intercept/public_access.go
@@ -1,6 +1,7 @@
 package intercept
 
 import (
+	"github.com/chaitin/koalaqa/model"
 	"github.com/chaitin/koalaqa/pkg/config"
 	"github.com/chaitin/koalaqa/pkg/context"
 	"github.com/chaitin/koalaqa/pkg/jwt"
@@ -12,14 +13,17 @@ type publicAccess struct {
 	intAuth Interceptor
 	jwt     *jwt.Generator
 	svcUser *svc.User
+
+	freeAuth bool
 }
 
 func newPublicAccess(auth *svc.Auth, cfg config.Config, generator *jwt.Generator, user *svc.User) Interceptor {
 	return &publicAccess{
-		svcAuth: auth,
-		intAuth: newAuth(cfg, generator, user),
-		jwt:     generator,
-		svcUser: user,
+		svcAuth:  auth,
+		intAuth:  newAuth(cfg, generator, user),
+		jwt:      generator,
+		svcUser:  user,
+		freeAuth: cfg.API.FreeAuth,
 	}
 }
 
@@ -37,9 +41,25 @@ func (p *publicAccess) Intercept(ctx *context.Context) {
 		return
 	}
 
-	userInfo, err := authUser(ctx, p.jwt, p.svcUser)
-	if err == nil {
-		ctx.SetUser(*userInfo)
+	if p.freeAuth {
+		user, err := p.svcUser.Admin(ctx)
+		if err != nil {
+			ctx.Unauthorized("get adimn failed")
+			ctx.Abort()
+			return
+		}
+
+		ctx.SetUser(model.UserInfo{
+			UID:      user.ID,
+			Username: user.Name,
+			Email:    user.Email,
+			Role:     user.Role,
+		})
+	} else {
+		userInfo, err := authUser(ctx, p.jwt, p.svcUser)
+		if err == nil {
+			ctx.SetUser(*userInfo)
+		}
 	}
 
 	ctx.Next()
