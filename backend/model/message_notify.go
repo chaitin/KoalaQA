758b0c99c5b201f970144f9aa343f082169f1db0
姚凯 <kai.yao@chaitin.com>
2025-09-12 16:18:37 +0800

    fix: notify_info add json tag

--- backend/model/message_notify.go
+++ backend/model/message_notify.go
@@ -26,13 +26,13 @@ func init() {
 }
 
 type MessageNotifyInfo struct {
-	DiscussID    uint          `gorm:"column:discussion_id"`
-	DiscussTitle string        `gorm:"disucss_title"`
-	Type         MsgNotifyType `gorm:"column:type"`
-	FromID       uint          `gorm:"column:from_id"`
-	FromName     string        `gorm:"column:from_name;type:text"`
-	FromBot      bool          `gorm:"column:from_bot"`
-	ToID         uint          `gorm:"column:to_id"`
-	ToName       string        `gorm:"column:to_name;type:text"`
-	ToBot        bool          `gorm:"to_bot"`
+	DiscussID    uint          `gorm:"column:discussion_id" json:"discuss_id"`
+	DiscussTitle string        `gorm:"disucss_title" json:"discuss_title"`
+	Type         MsgNotifyType `gorm:"column:type" json:"type"`
+	FromID       uint          `gorm:"column:from_id" json:"from_id"`
+	FromName     string        `gorm:"column:from_name;type:text" json:"from_name"`
+	FromBot      bool          `gorm:"column:from_bot" json:"from_bot"`
+	ToID         uint          `gorm:"column:to_id" json:"to_id"`
+	ToName       string        `gorm:"column:to_name;type:text" json:"to_name"`
+	ToBot        bool          `gorm:"to_bot" json:"to_bot"`
 }
