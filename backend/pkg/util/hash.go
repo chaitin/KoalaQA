471a7b198c3e4e14d7c623fa7d9f6d2a0e15981b
姚凯 <kai.yao@chaitin.com>
2025-09-15 16:37:13 +0800

    fix: hmac-sha256 move to util

--- backend/pkg/util/hash.go
+++ backend/pkg/util/hash.go
@@ -1,9 +1,19 @@
 package util
 
-import "crypto/md5"
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+)
 
 func MD5(data string) []byte {
 	hash := md5.New()
+	hash.Write([]byte(data))
+	return hash.Sum(nil)
+}
+
+func HMACSha256(secret string, data string) []byte {
+	hash := hmac.New(sha256.New, []byte(secret))
 	hash.Write([]byte(data))
 	return hash.Sum(nil)
 }
