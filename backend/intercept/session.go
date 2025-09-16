741eda71d06380c8a1d8d591a7fec9a50db849e0
姚凯 <kai.yao@chaitin.com>
2025-09-16 15:41:40 +0800

    fix: add csrf token

---
+++ backend/intercept/session.go
@@ -0,0 +1,38 @@
+package intercept
+
+import (
+	"github.com/chaitin/koalaqa/pkg/context"
+	"github.com/chaitin/koalaqa/pkg/util"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+type session struct {
+	handler gin.HandlerFunc
+}
+
+func newSession() Interceptor {
+	store := cookie.NewStore([]byte(util.RandomString(16)))
+	store.Options(sessions.Options{
+		Path:     "/",
+		MaxAge:   30,
+		HttpOnly: true,
+	})
+
+	return &session{
+		handler: sessions.Sessions("koala_session", store),
+	}
+}
+
+func (s *session) Intercept(ctx *context.Context) {
+	s.handler(ctx.Context)
+}
+
+func (s *session) Priority() int {
+	return -11
+}
+
+func init() {
+	registerGlobal(newSession)
+}
