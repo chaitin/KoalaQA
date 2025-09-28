80c9450d2f78d10ab7cb551f14b3e7835dfab65d
姚凯 <kai.yao@chaitin.com>
2025-09-28 16:28:54 +0800

    fix: remove doc old object

--- backend/pkg/util/path.go
+++ backend/pkg/util/path.go
@@ -21,3 +21,19 @@ func TrimFistDir(path string) string {
 
 	return strings.Join(split[1:], string(filepath.Separator))
 }
+
+func FirstDir(path string) string {
+	if path == "" {
+		return ""
+	}
+	split := strings.Split(path, string(filepath.Separator))
+	if split[0] == "" {
+		split = split[1:]
+	}
+
+	if len(split) < 2 {
+		return ""
+	}
+
+	return split[0]
+}
