8bcc30932fb887055970f2d8c1d74878526a17dd
xiaobing.wang <xiaobing.wang@chaitin.com>
2025-10-10 18:24:25 +0800

    feat: report

--- backend/server/fx.go
+++ backend/server/fx.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/chaitin/koalaqa/pkg/config"
-	"github.com/chaitin/koalaqa/pkg/report"
 	"github.com/chaitin/koalaqa/pkg/version"
 	"go.uber.org/fx"
 )
@@ -12,15 +11,11 @@ import (
 var Module = fx.Options(
 	fx.Provide(New),
 	fx.Invoke(func(lc fx.Lifecycle, e *Engine, v *version.Info, cfg config.Config) {
+		v.Print()
 		e.Init()
 
 		lc.Append(fx.Hook{
 			OnStart: func(ctx context.Context) error {
-				v.Print()
-
-				r := report.NewReport(v, cfg)
-				go r.ReportInstallation()
-
 				go e.Run()
 				return nil
 			},
