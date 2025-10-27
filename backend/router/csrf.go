8a2c53cbff59a04610a3fa470d89cc25044715f0
xiaobing.wang <xiaobing.wang@chaitin.com>
2025-10-27 12:13:35 +0800

    fix: csrf

--- backend/router/csrf.go
+++ backend/router/csrf.go
@@ -1,20 +1,22 @@
 package router
 
 import (
+	"github.com/chaitin/koalaqa/pkg/config"
 	"github.com/chaitin/koalaqa/pkg/context"
 	"github.com/chaitin/koalaqa/server"
 	ginCsrf "github.com/utrack/gin-csrf"
 )
 
 type csrf struct {
+	cfg config.Config
 }
 
 func (c *csrf) Route(h server.Handler) {
 	h.GET("/api/csrf", c.Get)
 }
 
-func newCsrf() server.Router {
-	return &csrf{}
+func newCsrf(cfg config.Config) server.Router {
+	return &csrf{cfg: cfg}
 }
 
 func init() {
@@ -29,5 +31,9 @@ func init() {
 // @Success 200 {object} context.Response{data=string}
 // @Router /csrf [get]
 func (c *csrf) Get(ctx *context.Context) {
+	if c.cfg.API.FreeCSRF {
+		ctx.Success("")
+		return
+	}
 	ctx.Success(ginCsrf.GetToken(ctx.Context))
 }
