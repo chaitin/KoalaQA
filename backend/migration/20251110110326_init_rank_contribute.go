e149c8ad1fc2bf3a968302f10acc62069e8ee4be
姚凯 <kai.yao@chaitin.com>
2025-11-10 11:05:26 +0800

    fix: add contribute rank init

---
+++ backend/migration/20251110110326_init_rank_contribute.go
@@ -0,0 +1,30 @@
+package migration
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"github.com/chaitin/koalaqa/migration/migrator"
+	"github.com/chaitin/koalaqa/repo"
+)
+
+type initRankContribute struct {
+	repoRank *repo.Rank
+}
+
+func (m *initRankContribute) Version() int64 {
+	return 20251110110326
+}
+
+func (m *initRankContribute) Migrate(tx *gorm.DB) error {
+	return m.repoRank.RefresContribute(context.Background())
+}
+
+func newInitRankContribute(rank *repo.Rank) migrator.Migrator {
+	return &initRankContribute{repoRank: rank}
+}
+
+func init() {
+	registerDBMigrator(newInitRankContribute)
+}
