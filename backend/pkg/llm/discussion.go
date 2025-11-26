b6f71c1431b527e8b39203d687b367ca374ef2ec
姚凯 <kai.yao@chaitin.com>
2025-11-26 16:52:16 +0800

    fix: update prompt

--- backend/pkg/llm/discussion.go
+++ backend/pkg/llm/discussion.go
@@ -87,7 +87,8 @@ const discussionFullTemplate = `
 var discussionsFullTemplate = template.New("discussions_full_template")
 
 const discussionsFullTemplateStr = `
-## 所有帖子信息
+## 帖子信息
+{{- if gt (len .) 0}}
 {{- range $j, $disc := .}}
 ### 帖子{{add $j 1}}
 #### 帖子ID：{{$disc.Discussion.ID}}
@@ -108,6 +109,9 @@ const discussionsFullTemplateStr = `
 {{- else}}
 暂无评论
 {{- end}}
+{{- end}}
+{{- else}}
+暂无信息
 {{- end}}
 `
 
