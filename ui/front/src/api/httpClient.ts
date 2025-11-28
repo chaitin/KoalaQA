4402f45ffa6783a077b0d963e8bec3b15f89ac98
xiaobing.wang <xiaobing.wang@chaitin.com>
2025-11-28 19:10:36 +0800

    fix: rag

--- ui/front/src/api/httpClient.ts
+++ ui/front/src/api/httpClient.ts
@@ -435,7 +435,7 @@ export class HttpClient<SecurityDataType = unknown> {
       headers: {
         ...((method &&
           this.instance.defaults.headers[
-            method.toLowerCase() as keyof HeadersDefaults
+          method.toLowerCase() as keyof HeadersDefaults
           ]) ||
           {}),
         ...(params1.headers || {}),
