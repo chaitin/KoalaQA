f37d956b85ee10b246022353f7c824eafb6335f7
姚凯 <kai.yao@chaitin.com>
2025-11-28 17:48:01 +0800

    fix: create qa by ai insight

--- backend/repo/rank.go
+++ backend/repo/rank.go
@@ -57,7 +57,7 @@ func (r *Rank) GroupByTime(ctx context.Context, rankLimit int, queryFuncs ...Que
 	opt := getQueryOpt(queryFuncs...)
 
 	err = r.db.WithContext(ctx).Table("(?) AS group_rank", r.model(ctx).
-		Select("id, score_id, foreign_id, extra, DATE_TRUNC('week',created_at) AS time, RANK() OVER ( PARTITION BY DATE_TRUNC('week',created_at) order by score*log(2, 1+hit) DESC, id ASC) AS rank").
+		Select("id, score_id, foreign_id, associate_id, extra, DATE_TRUNC('week',created_at) AS time, RANK() OVER ( PARTITION BY DATE_TRUNC('week',created_at) order by score*log(2, 1+hit) DESC, id ASC) AS rank").
 		Scopes(opt.Scopes()...),
 	).
 		Select("time, JSONB_AGG(jsonb_build_object('id', id,'score_id',score_id, 'foreign_id', foreign_id, 'associate_id', associate_id, 'extra', extra)) AS items").
