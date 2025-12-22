044151524ddaf8c96eccdb8a54b48793e69178c7
姚凯 <kai.yao@chaitin.com>
2025-12-22 10:54:55 +0800

    fix: week trunc

--- backend/pkg/util/time.go
+++ backend/pkg/util/time.go
@@ -12,8 +12,15 @@ func DayTrunc(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
+// WeekTrunc 周一才是一周的开始
 func WeekTrunc(t time.Time) time.Time {
-	monday := t.AddDate(0, 0, -int(t.Weekday()))
+	day := int(t.Weekday())
+
+	if day == 0 {
+		day = 7
+	}
+
+	monday := t.AddDate(0, 0, 1-day)
 	return DayTrunc(monday)
 }
 
