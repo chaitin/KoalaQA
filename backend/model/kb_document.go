c805f750922330515c1481609a64deaa4c46e7ca
姚凯 <kai.yao@chaitin.com>
2025-12-30 15:16:18 +0800

    fix: platform opt omit empty

--- backend/model/kb_document.go
+++ backend/model/kb_document.go
@@ -57,9 +57,9 @@ type PlatformOpt struct {
 	AppID        string `json:"app_id,omitempty"`
 	Secret       string `json:"secret,omitempty"`
 	AccessToken  string `json:"access_token,omitempty"`
-	RefreshToken string `json:"refresh_token"`
-	Username     string `json:"username"`
-	UserThirdID  string `json:"user_third_id"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+	Username     string `json:"username,omitempty"`
+	UserThirdID  string `json:"user_third_id,omitempty"`
 	Phone        string `json:"phone,omitempty"`
 }
 
