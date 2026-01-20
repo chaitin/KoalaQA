2bb3af32fa3e152026fa0cf8903f3c81f7db53d5
姚凯 <kai.yao@chaitin.com>
2026-01-20 15:35:09 +0800

    fix: user query

--- backend/repo/ask_session.go
+++ backend/repo/ask_session.go
@@ -15,7 +15,7 @@ func (a *AskSession) ListSession(ctx context.Context, res any, queryFuncs ...Que
 	opt := getQueryOpt(queryFuncs...)
 
 	return a.db.WithContext(ctx).Table("(?) AS records", a.db.Model(a.m).
-		Select("ask_sessions.*, users.name AS username, ROW_NUMBER() OVER (PARTITION BY ask_sessions.uuid, ask_sessions.user_id ORDER BY ask_sessions.created_at ASC) as rn").
+		Select("ask_sessions.*, COALESCE(users.name, '匿名游客') AS username, ROW_NUMBER() OVER (PARTITION BY ask_sessions.uuid, ask_sessions.user_id ORDER BY ask_sessions.created_at ASC) as rn").
 		Joins("LEFT JOIN users ON ask_sessions.user_id = users.id").Where("bot = ?", false),
 	).Where("rn = ?", 1).Scopes(opt.Scopes()...).Find(res).Error
 }
